880628806df55451ec9c0ed1bc6b3715e19a6471
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-08-05 20:54:39 +0530

    Working with SQLAlchemy and Static Files

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, flash, redirect , render_template, request, session , url_for
 # from datetime import timedelta
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer
 
 
 app = Flask(__name__);
 app.secret_key = "samplekeymessageoverhere"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.sqlite3';
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False;
 # app.permanent_session_lifetime = timedelta(daye = 4);
+
+database = SQLAlchemy(app)
+
+class users(database.Model):
+    _id = database.Column("id" , database.Integer , primary_key = True);
+    name = database.Column(database.String(200));
+    email = database.Column(database.String(200));
+
+    def __init__(self , name , email):
+        self.name = name;
+        self.email = email;
 
 @app.route('/')
 def home():
@@ -17,6 +31,15 @@ def login():
         # session.permanent = True;
         name = request.form["username"];
         session["user"] = name;
+
+        found_user = users.query.filter_by(name=name).first();
+        if found_user:
+            session['email'] = found_user.email;
+        else:
+            user = users(name , "");
+            database.session.add(user);
+            database.session.commit();
+
         flash("Login Successful")
         return redirect(url_for("user" , usr=name))
     else:
@@ -35,6 +58,11 @@ def user():
         if request.method == "POST":
             email = request.form["email"];
             session['email'] = email;
+            
+            found_user = users.query.filter_by(name=user).first();
+            found_user.email = email;
+            database.session.commit();
+
             flash("Emal Updated")
         else:
             if "email" in session:
@@ -45,6 +73,10 @@ def user():
         flash("You are not logged in")
         return redirect(url_for("login"));
 
+@app.route("/view")
+def view():
+    return render_template("view.html" , values = users.query.all())
+
 @app.route("/logout")
 def logout():
     flash("You have been logged out successfully!")
@@ -53,4 +85,5 @@ def logout():
     return redirect(url_for("login"));
 
 if __name__ == "__main__":
+    database.create_all();
     app.run(debug=True);
