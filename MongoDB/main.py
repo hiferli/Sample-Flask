76cd36ae8033d96f302ad05eb5dc9c1301cc8906
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-08-10 17:03:17 +0530

    Updating data in Database

--- MongoDB/main.py
+++ MongoDB/main.py
@@ -4,9 +4,11 @@ if __name__ == "__main__":
     print("Hello World. Welcome to PyMongo");
 
     client = pymongo.MongoClient("mongodb://localhost:27017");
-    collection = client['Sample'];
+    database = client['Sample']
+    collection = database['Sample Collection'];
 
-    previous_data = {"Name" : "Ramen"};
-    next_data = {"$set" : {"Intern" : True}};
 
-    collection.update_one(previous_data , next_data);
+    previous_data = {"Name" : "Ishaan"};
+    next_data = {"$set" : {"Name" : "Joshi"}};
+
+    collection.update_many(previous_data , next_data);
