3c9e267612afd04e790873b2b8e0659ca4f2ed62
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-08-13 17:39:07 +0530

    Error handling in APIs

--- AJAX/AJAX.js
+++ AJAX/AJAX.js
@@ -1,4 +1,5 @@
 let fetch = document.getElementById("fetchButton");
+
 fetch.addEventListener("click" , buttonClickHandler);
 
 function buttonClickHandler() {
@@ -27,34 +28,4 @@ function buttonClickHandler() {
 
     parameters_json = {"name":"testSample","salary":"123","age":"23"};
     xhr.send(parameters_json);
-}
-
-let data = document.getElementById("dataButton");
-data.addEventListener("click" , addEmployees);
-
-function addEmployees() {
-    console.log("Welcomd Here");
-
-    const xhr = new XMLHttpRequest();
-
-    xhr.open("GET" , "https://dummy.restapiexample.com/api/v1/employees" , true);
-    xhr.getResponseHeader("Content-type" , "application/json")
-
-    xhr.onload = function () {
-        if(this.status === 200){
-            let object = JSON.parse(this.responseText);
-            // console.log(object);
-
-            let list = document.getElementById("list");
-
-            for(key in object.data){
-                console.log(object.data[key].employee_name);
-                list.innerHTML += `<li>${object.data[key].employee_name}</l1>`;
-            }
-        } else {
-            console.error("Some Error");
-        }
-    }
-
-    xhr.send();
 }
--- REST API/main.py
+++ REST API/main.py
@@ -1,34 +1,42 @@
-from flask import Flask , jsonify
+from flask import Flask, jsonify , redirect , url_for
 
-app = Flask(__name__);
+app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return "Hello World"
 
+
 @app.route("/palindrome")
+@app.route("/palindrome/")
 def errorDrome():
     result = {
-        "Error" : True,
-        "Description" : "Please enter a value"
+        "Error": True,
+        "Description": "Please enter a value"
     }
 
-    return jsonify(result);
+    return jsonify(result)
 
 @app.route("/palindrome/<string:n>")
-def palindrome(n = 1):
-    if(n == n[::-1]):
-        result = {
-            "Number" : int(n),
-            "isPalindrome" : True
-        }
-    else:
-        result = {
-            "Number" : int(n),
-            "isPalindrome" : False
-        }
+def palindrome(n):
+    if (n != None):
+        if (n == n[::-1]):
+            result = {
+                "Number": int(n),
+                "isPalindrome": True
+            }
+        else:
+            result = {
+                "Number": int(n),
+                "isPalindrome": False
+            }
+    else :
+        return redirect(url_for("errorDrome"));
 
-    return jsonify(result);
-    
+
+    return jsonify(result)
+
+
 if __name__ == "__main__":
-    app.run(debug = True);
+    app.run(debug=True)
