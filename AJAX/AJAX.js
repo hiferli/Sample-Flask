95425371180c0dc31cf58412124e647f7a443334
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-08-13 14:19:24 +0530

    Data from API and put on Webpage using AJAX

--- AJAX/AJAX.js
+++ AJAX/AJAX.js
@@ -1,5 +1,4 @@
 let fetch = document.getElementById("fetchButton");
-
 fetch.addEventListener("click" , buttonClickHandler);
 
 function buttonClickHandler() {
@@ -28,4 +27,34 @@ function buttonClickHandler() {
 
     parameters_json = {"name":"testSample","salary":"123","age":"23"};
     xhr.send(parameters_json);
+}
+
+let data = document.getElementById("dataButton");
+data.addEventListener("click" , addEmployees);
+
+function addEmployees() {
+    console.log("Welcomd Here");
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET" , "https://dummy.restapiexample.com/api/v1/employees" , true);
+    xhr.getResponseHeader("Content-type" , "application/json")
+
+    xhr.onload = function () {
+        if(this.status === 200){
+            let object = JSON.parse(this.responseText);
+            // console.log(object);
+
+            let list = document.getElementById("list");
+
+            for(key in object.data){
+                console.log(object.data[key].employee_name);
+                list.innerHTML += `<li>${object.data[key].employee_name}</l1>`;
+            }
+        } else {
+            console.error("Some Error");
+        }
+    }
+
+    xhr.send();
 }
